2a3b3aa778129d137f711760b6074837e8ed8512
Dave Winer <dave.winer@gmail.com>
2016-03-27 13:32:30 -0400

    v0.92

--- storage.js
+++ storage.js
@@ -23,7 +23,7 @@
 	structured listing: http://scripting.com/listings/storage.html
 	*/
 
-var myVersion = "0.92d", myProductName = "nodeStorage"; 
+var myVersion = "0.92e", myProductName = "nodeStorage"; 
 
 var http = require ("http"); 
 var urlpack = require ("url");
